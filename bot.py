86afd0ad154628f50f4b1798223371af67936fa4
kuramin <kdv1543@gmail.com>
2024-10-06 20:51:09 +0200

    simple code 6

--- bot.py
+++ bot.py
@@ -9,10 +9,19 @@ bot = telebot.TeleBot(token=TOKEN)
 
 app = Flask(__name__)
 
-@bot.message_handler(commands=['start']) # welcome message handler
+@bot.message_handler(commands=['/start']) # welcome message handler
 def send_welcome(message):
     print(f"reply Hello Im bot to {message}") 
     bot.reply_to(message, 'Hello! I am bot')
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    try:
+        print(f"Received message: {message.text}")
+        bot.send_message(message.chat.id, "Hello, world!")
+        #bot.reply_to(message, "Hello, world!")
+    except telebot.apihelper.ApiTelegramException as e:
+        print('exception during send', e)
 
 # Handle all text messages sent to the bot
 #@bot.message_handler(func=lambda message: True)
@@ -45,7 +54,7 @@ def getMessage():
 
 # Webhook set-up (this is not strictly necessary but it's good practice)
 @app.route("/")  # defines what happens when https://telegram-bot-34zs.onrender.com is visited
-def webhook():
+def webhook():  # somehow it was also launched right after going live
     bot.remove_webhook()
     print(f"WEBHOOK_URL together with TOKEN is {WEBHOOK_URL}/{TOKEN}")
     bot.set_webhook(url=f"{WEBHOOK_URL}/{TOKEN}")
