057778517294b3b7e6b1d394a5a1a1669effa57f
kuramin <kdv1543@gmail.com>
2024-10-07 00:20:43 +0200

    simple code 7

--- bot.py
+++ bot.py
@@ -9,17 +9,19 @@ bot = telebot.TeleBot(token=TOKEN)
 
 app = Flask(__name__)
 
-@bot.message_handler(commands=['/start']) # welcome message handler
-def send_welcome(message):
-    print(f"reply Hello Im bot to {message}") 
-    bot.reply_to(message, 'Hello! I am bot')
+
+
+#@bot.message_handler(commands=['start']) # welcome message handler. Works with start command only
+#def send_welcome(message):  # this name can be anything
+#    print(f"reply Hello Im bot to {message}") 
+#    bot.reply_to(message, 'Hello! I am bot')
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     try:
         print(f"Received message: {message.text}")
-        bot.send_message(message.chat.id, "Hello, world!")
-        #bot.reply_to(message, "Hello, world!")
+        #bot.send_message(message.chat.id, "Hello, world!")
+        bot.reply_to(message, "Hello, world! is what I say to every message")
     except telebot.apihelper.ApiTelegramException as e:
         print('exception during send', e)
 
@@ -41,20 +43,23 @@ def echo_all(message):
 
 # For webhook support: Use Flask to handle Telegram webhook
 @app.route('/' + TOKEN, methods=['POST'])
-def getMessage():
+#@app.route('/webhook', methods=['POST'])
+def webhook():
     try:
         json_str = request.stream.read().decode('UTF-8')
         print(f"json_str is {json_str}")
         update = telebot.types.Update.de_json(json_str)
         bot.process_new_updates([update])
         print("Webhook data processed successfully")
+        return "!", 200
     except Exception as e:
         print(f"Error processing webhook: {e}")
-    return "!", 200
+        return e
+    
 
 # Webhook set-up (this is not strictly necessary but it's good practice)
-@app.route("/")  # defines what happens when https://telegram-bot-34zs.onrender.com is visited
-def webhook():  # somehow it was also launched right after going live
+#@app.route("/webhook", methods=["POST"])  # defines what happens when https://telegram-bot-34zs.onrender.com/webhook is visited
+def webhook_update():  # somehow it was also launched right after going live
     bot.remove_webhook()
     print(f"WEBHOOK_URL together with TOKEN is {WEBHOOK_URL}/{TOKEN}")
     bot.set_webhook(url=f"{WEBHOOK_URL}/{TOKEN}")
@@ -62,4 +67,5 @@ def webhook():  # somehow it was also launched right after going live
     return "Webhook set", 200
 
 if __name__ == "__main__":
+    webhook_update()
     app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)), debug=True)
