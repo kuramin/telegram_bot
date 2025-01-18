5d08be26645847fb5abd0bb112ba30047e6f5624
kuramin <kdv1543@gmail.com>
2025-01-18 21:55:25 +0100

    doubled remove webhook

--- bot.py
+++ bot.py
@@ -60,6 +60,14 @@ def reset_webhook():  # somehow it was also launched right after going live
     bot.set_webhook(url=f"{WEBHOOK_URL}/{TOKEN}")
     print("Webhook was reset")
     return "Webhook set", 200
+    
+@app.route("/webhook", methods=["GET"])
+def reset2_webhook():  # somehow it was also launched right after going live
+    #bot.remove_webhook()
+    print(f"WEBHOOK_URL together with TOKEN is {WEBHOOK_URL}/{TOKEN}")
+    bot.set_webhook(url=f"{WEBHOOK_URL}/{TOKEN}")
+    print("Webhook was reset")
+    return "Webhook set", 200
 
 if __name__ == "__main__":
     reset_webhook()
